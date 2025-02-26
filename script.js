d9a384109869ce3ef0f102a6b7fa3941df0294e5
ravindradevarapalli <130473043+ravindradevarapalli@users.noreply.github.com>
2025-02-26 18:12:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+if (registerBtn && loginBtn && container) {
+  registerBtn.addEventListener("click", () => {
+    container.classList.add("active");
+  });
+
+  loginBtn.addEventListener("click", () => {
+    container.classList.remove("active");
+  });
+} else {
+  console.error("Elements not found in the DOM.");
+}
